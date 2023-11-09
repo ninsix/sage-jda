7bbf3e50e0f6c1a2ff4a22a8f15e6226b6c58d44
Ninsix <132847620+ninsix@users.noreply.github.com>
2023-11-09 13:12:36 -0500

    Sage 0.2.0 released
    - Commands updated
    - Now Sage uses ShadowJar to reduce bloat
    - Is now possible to run the Jar directly (not using scripts)

--- sage/src/main/java/ninsix/sage/core/Main.java
+++ sage/src/main/java/ninsix/sage/core/Main.java
@@ -17,7 +17,7 @@ import static ninsix.sage.core.DataLoader.OS_NAME;
 public class Main {
 
     public static final String NAME = "Sage";
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.2.0";
 
     public static void main(String[] args) {
         final String token = getToken(args);
